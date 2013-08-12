1ba8466d8889828a490cd4792a96935f73ffc43b
Paul Reioux <reioux@gmail.com>
2013-08-12 14:36:01 -0500

    GPU Overclock: initial GPU overclocking to 487.5 MHz

--- build_m7_kernel_bryan.sh
+++ build_m7_kernel_bryan.sh
@@ -27,7 +27,7 @@
   m=$k/out/$c/system/lib/modules
   z=$c-$today
 
-TOOLCHAIN=/home/brymaster5000/android/kernel/arm-eabi-4.7/bin/arm-eabi-
+TOOLCHAIN=/home/brymaster5000/android/kernel/Linaro-4.9.1/bin/arm-cortex_a15-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
 
