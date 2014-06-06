f81368b021f20edb4d4646f4fc0ec221261d510e
flar2 <asegaert@gmail.com>
2014-06-05 22:43:32 -0400

    Linaro GCC 4.9.1: fix compilation errors

--- net/socket.c
+++ net/socket.c
@@ -1178,7 +1178,7 @@ EXPORT_SYMBOL(sock_create_kern);
 SYSCALL_DEFINE3(socket, int, family, int, type, int, protocol)
 {
 	int retval;
-	struct socket *sock;
+	struct socket *sock = NULL;
 	int flags;
 
 	
@@ -1216,7 +1216,7 @@ out_release:
 SYSCALL_DEFINE4(socketpair, int, family, int, type, int, protocol,
 		int __user *, usockvec)
 {
-	struct socket *sock1, *sock2;
+	struct socket *sock1 = NULL, *sock2;
 	int fd1, fd2, err;
 	struct file *newfile1, *newfile2;
 	int flags;
