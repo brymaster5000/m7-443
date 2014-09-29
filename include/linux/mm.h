662f81eff8bad561743603db0ccd2333fbb6c502
Tom Tartaro <tomtartaro@gmail.com>
2014-09-29 14:38:25 -0700

    assorted tweaks
    Conflicts:
    	include/linux/mm.h

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1130,8 +1130,9 @@ extern int filemap_fault(struct vm_area_struct *, struct vm_fault *);
 int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
-#define VM_MAX_READAHEAD	128	
-#define VM_MIN_READAHEAD	16	
+/* readahead.c */
+#define VM_MAX_READAHEAD	1024	/* kbytes */
+#define VM_MIN_READAHEAD	64	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
