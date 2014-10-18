ac3acbc2164ea58c25bd9e47cfa2b834658e4a91
brymaster5000 <brymaster5000@gmail.com>
2014-10-18 19:19:35 -0400

    cpufreq: add slim governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -336,6 +336,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
+extern struct cpufreq_governor cpufreq_gov_slim;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_slim)
 #endif
 
 
